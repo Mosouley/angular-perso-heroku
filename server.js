cbd7e5b53d996992b4e2fe79b5f11bf490f05c47
mosouley <mo.souley@gmail.com>
2021-09-25 16:38:20 +0300

    changes to the main created

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// function requireHTTPS(req, res, next) {
+clear// function requireHTTPS(req, res, next) {
 //   // The 'x-forwarded-proto' check is for Heroku
 //   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
 //       return res.redirect('https://' + req.get('host') + req.url);
