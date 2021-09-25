0357196ef798cd451c326d595fa06a863984a2da
mosouley <mo.souley@gmail.com>
2021-09-25 17:04:31 +0300

    corrections on server.js path

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/dist/<personal-portfolio>'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/<personal-portfolio>/index.html'));
+res.sendFile(path.join(__dirname + '/dist/<personal-portfolio>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
