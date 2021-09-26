e64dd35bcfc165465654a2dd4efc9949b78dcfe9
mosouley <mo.souley@gmail.com>
2021-09-26 11:39:06 +0300

    clearing server.js file for deployment

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-clear// function requireHTTPS(req, res, next) {
-//   // The 'x-forwarded-proto' check is for Heroku
-//   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-//       return res.redirect('https://' + req.get('host') + req.url);
-//   }
-//   next();
-// }
+function requireHTTPS(req, res, next) {
+ //The 'x-forwarded-proto' check is for Heroku
+ if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+     return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+ }
 
 
 //Install express server
